4aadfb71122445e4e4cd18d1bb4bd0cb6677af8b
Kyle Thomas <thomaskcr@gmail.com>
2015-04-11 23:23:56 -0400

    Get user's guess for value of dice roll.

--- dice_game.py
+++ dice_game.py
@@ -2,3 +2,10 @@ import random
 
 # Roll the dice and get a number between 1 and 6
 dice_number = random.randrange(1, 7)
+
+# Get user's guess from input
+print("Guess the number rolled on the six sided dice:")
+user_guess = input()
+
+# Convert the user's guess to an integer
+user_guess = int(user_guess)
